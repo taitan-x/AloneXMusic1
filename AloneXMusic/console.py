12540b4c611072517a9c20c6bafa2a1409c1dc0d
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-07-22 09:06:33 +0530

    Add files via upload

--- AloneXMusic/console.py
+++ AloneXMusic/console.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
-from SankiMusic.utilities.config import LOG_FILE_NAME
+from AloneXMusic.utilities.config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
